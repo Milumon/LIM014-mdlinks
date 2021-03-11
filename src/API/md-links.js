b8f1d124349ca45a4a3a7c51d9d31e5c3f3faa63
Milumon <17993682+Milumon@users.noreply.github.com>
2021-03-11 06:29:33 -0500

    Update Readme

---
+++ src/API/md-links.js
@@ -0,0 +1,14 @@
+const options = require('./options.js');
+
+/*modulo para las funciones referentes a las rutas*/
+
+const mdLinks = (path, options = { validate: true}) => new Promise ((resolve, reject){
+
+
+
+
+
+}) 
+ 
+
+module.exports = { mdLinks };
