aa3497ff1284f7902b1733140cb4bfabed13fff2
Milumon <17993682+Milumon@users.noreply.github.com>
2021-03-16 04:18:02 -0500

    features #7

---
+++ README copy 2.md
@@ -0,0 +1,38 @@
+# Markdown Links
+
+## Índice
+
+* [1. Resumen](#1-resumen)
+* [2. Diagrama de flujo](#2-diagrama-de-flujo)
+* [3. Product Backlog](#3-product-backlog) 
+
+***
+
+## 1. Resumen
+
+[Node.js](https://nodejs.org/es/) es un entorno de ejecución para JavaScript
+construido con el [motor de JavaScript V8 de Chrome](https://developers.google.com/v8/).
+Esto nos va a permitir ejecutar JavaScript en el entorno del sistema operativo,
+ya sea tu máquina o un servidor, lo cual nos abre las puertas para poder
+interactuar con el sistema en sí, archivos, redes, ...
+
+En este proyecto nos alejamos un poco del navegador para construir un programa
+que se ejecute usando Node.js, donde aprenderemos sobre cómo interactuar con el
+sistema archivos, con el entorno (_proceso_, _env_, _stdin/stdout/stderr_), ...
+
+En este proyecto crearás una herramienta de línea de comando (CLI) así como tu
+propia librería (o biblioteca - library) en JavaScript.
+ 
+## 2. Diagrama de flujo
+
+### API
+  ![DiagramaAPI](img/Diagrama1.png)
+### CLI
+  ![DiagramaCLI](img/Diagrama2.png)
+
+
+## 3. Product Backlog
+
+* Issues del proyecto
+
+  *  Diagrama de flujo
--- src/API/methods.js
+++ src/API/methods.js
@@ -6,27 +6,50 @@ const path = require('path');
 // función que recibe una ruta y verifica si es absoluta, retorna un booleano
 const isAbsolute = (route) => path.isAbsolute(route);
 // función que resuelve una ruta relativa a absoluta
-const resolveRelative = (route) => path.resolve(route);
+// const resolveToAbsolute = (route) => path.resolve(route);
 // función sincrona que recibe una ruta y verifica si es un directorio, retorna un boolean.
-const isDirectory = (route) => (fs.statSync(route).isDirectory);
+const isDirectory = (route) => fs.statSync(route).isDirectory();
 // función sincrona que recibe una ruta y verifica si es un archivo, retorna un boolean.
-const isFile = (route) => (fs.statSync(route).isFile);
+
+const validAndResolve = ((route) => {
+  if (!isAbsolute(route)) {
+    const resolveToAbsolute = path.resolve(route);
+    return resolveToAbsolute;
+  }
+  return route;
+});
+
+const isFileAndExists = (route) => {
+  if (fs.statSync(route).isFile()) {
+    return fs.existsSync(route);
+  }
+  return false;
+};
 // función que verifica si es un archivo .md
 const isMarkdown = (route) => (path.extname(route) === '.md');
 // función que verifica si la ruta existe, retorna un boolean.
-const isValidRoute = (route) => fs.existsSync(route);
+// const isValidRoute = (route) => fs.existsSync(route);
 // función que recorre un directorio y almacena en un array
+
 // la ruta de los archivos dentro del directorio. Devuelve un array de rutas.
 const arrayDirectory = (route) => fs.readdirSync(route);
+
 // función que lee el contenido de un archivo y lo transforma a string
 const stringFileContent = (route) => fs.readFileSync(route);
+// función que resuelve rutas
+// const normalize = (route) => path.normalize(route);
 
 // exportar funciones para poder llamarlas desde otro archivo
 module.exports = {
-  isAbsolute,
-  resolveRelative,
+  // isAbsolute,
+  // resolveToAbsolute,
   isMarkdown,
   isDirectory,
-  isFile,
-  isValidRoute,
+  // isFile,
+  // isValidRoute,
+  arrayDirectory,
+  stringFileContent,
+  // normalize,
+  validAndResolve,
+  isFileAndExists,
 };
